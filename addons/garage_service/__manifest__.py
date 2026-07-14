c2a8f8c19d34b0e107ab0b4378c54f75fcdc3138
Andrii Chychkan <mywork1728@gmail.com>
2026-07-14 14:31:02 +0300

    [IMP] garage_service: add ir.model.access

--- addons/garage_service/__manifest__.py
+++ addons/garage_service/__manifest__.py
@@ -10,7 +10,11 @@
     "depends": [
         "base",
     ],
-    "data": [],
+    "data": [
+        "security/ir.model.access.csv",
+        
+        "views/garage_menu_views.xml",
+    ],
     "application": True,
     "author": "Angry andrii",
     "license": "LGPL-3",
